aff0b7648f98f3c2f4ea9e4ab3cd95a82366ddb6
akinmaurice <akinmaurice@gmail.com>
2018-12-14 22:44:17 +0100

    Add test for create data for key

--- test/unit/create.data.test.js
+++ test/unit/create.data.test.js
@@ -2,6 +2,8 @@ import chai from 'chai';
 import sinon from 'sinon';
 import rewire from 'rewire';
 
+const mongoose = require('mongoose');
+
 const Key = require('../../app/models/Key');
 const Data = require('../../app/models/Data');
 
@@ -36,5 +38,30 @@ describe('It validates all Functions to create data for key', () => {
         const checkRequest = createKeyData.__get__('checkRequest');
         const response = await checkRequest(request);
         response.should.equal(true);
+    });
+
+
+    it('Should verify Key Exists', async() => {
+        const key_name = 'akin-key';
+        const data = {
+            id: 'jfjfj'
+        };
+        sandbox.stub(Key, 'findOne').returns(Promise.resolve(data));
+        const verifyKey = createKeyData.__get__('verifyKey');
+        const response = await verifyKey(key_name);
+        response.should.equal(data);
+    });
+
+
+    it('Should create data for key', async() => {
+        const key_name = 'akin-key';
+        const data = {
+            key: key_name,
+            message: 'hello World'
+        };
+        sandbox.stub(Data, 'create').returns(Promise.resolve('Data Added'));
+        const createData = createKeyData.__get__('createData');
+        const response = await createData(data, key_name);
+        response.should.equal('Data Added');
     });
 });
